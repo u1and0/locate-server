8a6d740c90434645b349b81d6ab0e9b2bdda578e
u1and0 <e01.ando60@gmail.com>
2019-08-17 08:26:36 +0900

    [add] logfile

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func main() {
 	if err != nil {
 		log.Println("[warning] cannot open logfile" + err.Error())
 	}
+	defer logfle.Close()
 	log.SetOutput(io.MultiWriter(logfile, os.Stdout))
 
 	// Initialize cache

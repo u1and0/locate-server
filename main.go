bfacb57fb99e657b57db1ee28ef537e33e98d40e
u1and0 <e01.ando60@gmail.com>
2020-10-30 20:47:43 +0900

    [add] 部分文字列検索 for search window
    See also
    [jQuery event for HTML5 datalist when item is selected or typed input match with item in the list](https://stackoverflow.com/questions/30151633/jquery-event-for-html5-datalist-when-item-is-selected-or-typed-input-match-with)

--- main.go
+++ main.go
@@ -355,6 +355,8 @@ func addResult(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, htmlClause(receiveValue))
 		fmt.Fprintf(w, ` %s
 						</h4>
+					<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+					<script type="text/javascript" src="datalist.js"></script>
 					</body>
 					</html>`, err)
 	} else { // 検索文字数チェックOK

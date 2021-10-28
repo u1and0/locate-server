a30aa9b8f1b3ec484610f125ef94a4159ec044d0
u1and0 <e01.ando60@gmail.com>
2021-10-28 22:10:10 +0900

    [add] construct table script @parsejson.js

--- static/parsejson.js
+++ static/parsejson.js
@@ -2,8 +2,14 @@ async function main(){
   try {
     const query = document.getElementById("q").value;
     const resultPath = await fetchLocatePath(query);
+    displayView(resultPath);
+
+    /*
     const view = createView(resultPath);
+    console.log(view);
     displayView(view);
+    */
+
     // .then((userInfo) => createView(userInfo)) // JSONオブジェクトで解決されるPromise
     // .then((view)=> displayView(view))  // HTML文字列で解決されるPromise
   } catch(error) {
@@ -24,38 +30,29 @@ function fetchLocatePath(query){
       } else{
         return response.json(); //.then(userInfo =>  ここはmain()で解決
       }
-      console.log(query);
     });
-}
-//
-// HTMLの組立
-function createView(resultPath){
-  // タグ関数escapeHTMLの呼び出しは``の前
-  return escapeHTML`
-  <h4>${resultPath.query}</h4>
-  <table>
-      <tr>
-        <td>${resultPath.paths[0]}</td>
-      </tr>
-  </table>
-  `;
 }
 
 // HTMLの挿入
 function displayView(view){
-  const result = document.getElementById("result");
-  result.innerHTML = view;
+  const table = document.getElementById("result");
+  // result.innerHTML = view;
+  view.paths.forEach((p) =>{
+      let newRow = table.insertRow(-1);
+      let newCell = newRow.insertCell(0);
+      newCell.appendChild(document.createTextNode(p));
+  });
 }
 
-// escapeHTML``から呼び出される
-function escapeSpecialChars(str){
-  return str
-  .replace(/&/g, "&amp;")
-  .replace(/</g, "&lt;")
-  .replace(/>/g, "&gt;")
-  .replace(/"/g, "&quot;")
-  .replace(/'/g, "&#039;");
-}
+// // escapeHTML``から呼び出される
+// function escapeSpecialChars(str){
+//   return str
+//   .replace(/&/g, "&amp;")
+//   .replace(/</g, "&lt;")
+//   .replace(/>/g, "&gt;")
+//   .replace(/"/g, "&quot;")
+//   .replace(/'/g, "&#039;");
+// }
 
 // テンプレートリテラルをタグ付けすることで、
 // 明示的にエスケープ用の関数を呼び出す必要がないようにする

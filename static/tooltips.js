2686978e827f5b4b827b054f908b7ba1399a117f
u1and0 <e01.ando60@gmail.com>
2021-11-12 00:38:59 +0900

    [add] loading animation

--- static/tooltips.js
+++ static/tooltips.js
@@ -2,3 +2,18 @@ function toggleMenu(elem){
   const obj = document.getElementById(elem).style;
   obj.display=(obj.display=='none')?'block':'none';
 }
+
+// Loading spinner
+$(function(){
+  var loader = $('.loader-wrap');
+
+  //ページの読み込みが完了したらアニメーションを非表示
+  $('#result').on('load',function(){
+    loader.fadeOut();
+  });
+
+  //ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
+  setTimeout(function(){
+    loader.fadeOut();
+  },5000);
+});

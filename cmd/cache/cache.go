e0a19bb36c923b7a5dab3765c96bab5dc083a335
u1and0 <e01.ando60@gmail.com>
2021-12-12 08:45:38 +0900

    [mod] main() split function

--- cmd/cache/cache.go
+++ cmd/cache/cache.go
@@ -5,10 +5,10 @@ import (
 )
 
 type (
-	// CacheMap is normalized queries key and PathMap value pair
-	CacheMap map[Key]*cmd.Paths
+	// Map is normalized queries key and PathMap value pair
+	Map map[Key]*cmd.Paths
 
-	// Key : Key for CacheMap
+	// Key : Key for Map
 	Key struct {
 		Word  string
 		Limit int
@@ -16,13 +16,13 @@ type (
 )
 
 // New : cache constructor
-func New() *CacheMap {
-	return &CacheMap{}
+func New() *Map {
+	return &Map{}
 }
 
 // Traverse : 検索結果をcacheの中から探し、あれば検索結果と検索数を返し、
 // なければLocater.Cmd()を走らせて検索結果と検索数を得る
-func (cache *CacheMap) Traverse(l *cmd.Locater) (paths cmd.Paths, ok bool, err error) {
+func (cache *Map) Traverse(l *cmd.Locater) (paths cmd.Paths, ok bool, err error) {
 	s := Key{cmd.Normalize(l.SearchWords, l.ExcludeWords), l.Query.Limit}
 	var v *cmd.Paths
 	if v, ok = (*cache)[s]; !ok {

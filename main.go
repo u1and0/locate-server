2967ecdbcf09ce4f3de1cab3466147246f8d6854
u1and0 <e01.ando60@gmail.com>
2021-12-13 15:34:20 +0900

    [mod] version info

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	// VERSION : version
-	VERSION = "3.0.0r"
+	VERSION = "3.1.0"
 	// LOGFILE : 検索条件 / 検索結果 / 検索時間を記録するファイル
 	LOGFILE = "/var/lib/mlocate/locate.log"
 	// LOCATEDIR : locate (gocate) search db path

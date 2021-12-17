e1f17cc7855b626f1e6db5edc114a45b87b01471
u1and0 <e01.ando60@gmail.com>
2021-12-17 12:46:52 +0900

    [mod] split main.js

--- static/main.js
+++ static/main.js
@@ -9,68 +9,17 @@ function main(){
   }
 }
 
-class Locater {
-  constructor(json){
-    this.args = json.args;  // command line argument
-    this.query = json.query;  // API args
-    this.searchWords = json.searchWords;  // search word for searching
-    this.excludeWords = json.excludeWords;  // exclude word for searching
-    this.paths = json.paths;  // result of locate command
-    this.stats = json.stats;  // stats info at database
-    this.error = json.error; // Error message
-  }
-
-  static displayStats(str){
-    const divElem = document.getElementById("search-status");
-    const newElem = document.createElement("b");
-    newElem.textContent = str;
-    divElem.appendChild(newElem);
-    const br = document.createElement("br");
-    divElem.appendChild(br);
-  }
-
-  // 検索パス表示
-  displayRoll(n, shift){
-    const folderIcon = '<i class="far fa-folder-open" title="クリックでフォルダを開く"></i>';
-    const sep = this.args.pathSplitWin ? "\\" : "/";
-    const dataArray = this.paths.slice(n, n + shift);
-    dataArray.forEach((p) =>{
-      const modified = this.pathModify(p);
-      const highlight = this.highlightRegex(modified);
-      const dir = Locater.dirname(modified, sep);
-      let result = `<a href="file://${modified}">${highlight}</a>`;
-      result += `<a href="file://${dir}"> ${folderIcon} </a>`;
-      $("#result").append("<tr><td>" + result + "</td></tr>");
+// fetchの返り値のPromiseを返す
+function fetchLocatePath(url){
+  return fetch(url)
+    .then(response =>{
+      // if (!response.ok) {
+        // return Promise.reject(new Error(`{${response.status}: ${response.statusText}`));
+      // } else{
+      return response.json();
     });
-  }
-
-  pathModify(str){
-    if (str.startsWith(this.args.trim)){
-      str = str.slice(this.args.trim.length);
-    }
-    if (this.args.pathSplitWin){
-      str = str.replaceAll("/", "\\");
-    }
-    if (this.args.root){
-      str = this.args.root + str;
-    }
-    return str;
-  }
-
-  highlightRegex(str){
-    this.searchWords.forEach((q) =>{
-      const re = new RegExp(q, "i"); // second arg "i" for ignore case
-      // $&はreのマッチ結果
-      str = str.replace(re, "<span style='background-color:#FFCC00;'>$&</span>");
-    })
-    return str;
-  }
-
-  static dirname(str, sep){
-    const idx = str.lastIndexOf(sep); // sep == "/" or "\\"
-    return str.slice(0,idx);
-  }
 }
+
 
 async function fetchSearchHistory(url){
   try{
@@ -123,24 +72,3 @@ async function fetchJSONPath(url){
   }
 }
 
-// class LocaterError extends Error{
-//   constructor(response){
-//     const jsonobj = response.json();
-//     super(`${response.status} for ${response.url}`);
-//     console.log(jsonobj);
-//     this.msg = jsonobj.error;
-//     this.name = 'LocaterError';
-//     this.response = response;
-//   }
-// }
-
-// fetchの返り値のPromiseを返す
-function fetchLocatePath(url){
-  return fetch(url)
-    .then(response =>{
-      // if (!response.ok) {
-        // return Promise.reject(new Error(`{${response.status}: ${response.statusText}`));
-      // } else{
-      return response.json();
-    });
-}
